e6e700f892205c6766347cc3718829314db07269
jsonjosn <jsonjosnflb@gmail.com>
2020-02-15 00:24:32 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const apiMocker = require('mocker-api');
+
+const app = express();
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type,Content-Length, Authorization, Accept,X-Requested-With'
+  );
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  res.header('X-Powered-By', ' 3.2.1');
+  if (req.method == 'OPTIONS') res.send(200);
+  /*让options请求快速返回*/ else next();
+});
+apiMocker(app, path.resolve('./mocker/mocker.js'));
+app.listen(3000, () => {
+  console.log('start at 3000');
+});
